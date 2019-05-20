fb6df17a1e164117b15c7a7c22f17f5c1656d239
Dan Fritchman <daniel.fritchman@gmail.com>
2019-05-20 15:59:20 +0200

    Add Yarn Stuff

---
+++ index.js
@@ -0,0 +1,48 @@
+
+var protobuf = require("protobufjs");
+
+
+const AddressBook = (async function (){ 
+    return await protobuf.load("addressbook.proto", function(err, root) {
+    if (err)
+        throw err;
+ 
+    // Obtain a message type
+    var AddressBook = root.lookupType("tutorial.AddressBook");
+    return AddressBook; 
+ 
+    // Exemplary payload
+    //var payload = { awesomeField: "AwesomeString" };
+ 
+    // Verify the payload if necessary (i.e. when possibly incomplete or invalid)
+    //var errMsg = AwesomeMessage.verify(payload);
+    //if (errMsg)
+        //throw Error(errMsg);
+ 
+    // Create a new message
+    //var message = AwesomeMessage.create(payload); 
+    // or use .fromObject if conversion is necessary
+ 
+    // Encode a message to an Uint8Array (browser) or Buffer (node)
+    //var buffer = AwesomeMessage.encode(message).finish();
+    // ... do something with buffer
+ 
+    // Decode an Uint8Array (browser) or Buffer (node) to a message
+    //var message = AwesomeMessage.decode(buffer);
+    // ... do something with message
+ 
+    // If the application uses length-delimited buffers, 
+    // there is also encodeDelimited and decodeDelimited.
+ 
+    // Maybe convert the message back to a plain object
+    //var object = AwesomeMessage.toObject(message, {
+        //longs: String,
+        //enums: String,
+        //bytes: String,
+        // see ConversionOptions
+    //});
+})})();
+
+console.log(AddressBook);
+
+
